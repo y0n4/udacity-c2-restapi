fe3a45a7d7c0fa611bcf3b7efbf4e517fae30550
y0n4 <04l.yona@gmail.com>
2019-07-26 16:21:14 -0400

    Hook up my aws postgres credentials

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagrampostgres",
+    "password": "udagrampostgres",
+    "database": "udagrampostgres",
+    "host": "udagrampostgres.crks3fohnav1.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_reigion": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagrams3"
   },
   "prod": {
     "username": "",
